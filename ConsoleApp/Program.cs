2ef834e139f9e825cf915e12003c70f5cee4892e
Herik dos Santos Lorenção <heriksantoslorencao@gmail.com>
2022-02-08 11:06:59 -0300

    introdução aos testes

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Alura.LeilaoOnline.Core;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var leilao = new Leilao("Van Gogh");
+            var fulano = new Interessada("Fulano", leilao);
+            var maria = new Interessada("Maria", leilao);
+
+            leilao.RecebeLance(fulano, 700);
+            leilao.RecebeLance(maria, 800);
+            leilao.RecebeLance(fulano, 1000);
+            leilao.RecebeLance(maria, 990);
+
+            leilao.TerminaPregao();
+            
+            Console.WriteLine(leilao.Ganhador.Valor);
+        }
+    }
+}
