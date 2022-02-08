900406e12926b178618ab55accb7c16e4a57b3b7
Herik dos Santos Lorenção <heriksantoslorencao@gmail.com>
2022-02-08 16:11:32 -0300

    adicionando validação de exceção no construtor

---
+++ Tests/LanceCtor.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Alura.LeilaoOnline.Core;
+using Xunit;
+
+namespace Tests
+{
+    public class LanceCtor
+    {
+        [Fact]
+        public void LancaArgumentExceptionQuandoValorNegativo()
+        {
+            var exception = Assert.Throws<ArgumentException>(() => new Lance(null, -100));
+            Assert.Equal("Informe um valor positivo", exception.Message);
+        }
+    }
+}
